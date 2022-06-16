403abc36ad500bd337e3ecb8181f169967d66e35
Kilmanas <m.kilmanas@gmail.com>
2022-06-16 18:06:09 +0300

    mid init

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -7,13 +7,5 @@ use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    public function create(Request $request)
-    {
-        $name = $request->post('company');
-        $company = Company::firstOrCreate(
-            ['name' => $name],
-            ['address' => $request->post('address')],
-            ['phone' => $request->post('phone')]
-        );
-    }
+
 }
